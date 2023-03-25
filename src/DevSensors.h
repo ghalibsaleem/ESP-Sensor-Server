1400c37b8af498f3f0bf9dd58e9a503fe7df0170
Ghalib Saleem <ghalib.saleem.123@gmail.com>
2023-03-24 21:51:18 -0400

    Base Commit

---
+++ src/DevSensors.h
@@ -0,0 +1,92 @@
+#include <DHT.h>
+#include "Adafruit_VEML7700.h"
+#include <Adafruit_Sensor.h>
+#include "Adafruit_SGP30.h"
+
+#define DHTPIN 4  
+#define DHTTYPE    DHT22 
+
+#define SDA_1 15
+#define SCL_1 16
+#define I2C_FREQ_1 400000
+
+#define SDA_2 17
+#define SCL_2 18
+#define I2C_FREQ_2 400000
+
+DHT dht(DHTPIN, DHTTYPE);
+
+struct SensorStatus {   
+  float temperature;           
+  float humidity;     
+  float eco2;  
+  float tvoc;
+  float ambientlight;
+  String airquality;
+} currentStatus;
+
+
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
+TwoWire I2C_1 = TwoWire(0);
+
+Adafruit_SGP30 sgp;
+TwoWire I2C_2 = TwoWire(1);
+
+void setupDHTSensor(){
+	dht.begin();
+	
+}
+
+void setupSGP(){
+	I2C_2.begin(SDA_2, SCL_2, I2C_FREQ_2);
+	if (! sgp.begin(&I2C_2)){
+		Serial.println("SGP30 Sensor not found :(");
+		while (1);
+	}
+	Serial.print("Found SGP30 serial #");
+	Serial.print(sgp.serialnumber[0], HEX);
+	Serial.print(sgp.serialnumber[1], HEX);
+	Serial.println(sgp.serialnumber[2], HEX);
+}
+
+
+void setupLuxSensorAuto(){
+    I2C_1.begin(SDA_1, SCL_1, I2C_FREQ_1);
+    if (!veml.begin(&I2C_1)) {
+        Serial.println("VEML7700 Auto Lux Sensor not found");
+        while (1){
+            delay(500);
+        }
+    }
+}
+
+float getLuxDataAuto(){
+    float lux = veml.readLux(VEML_LUX_AUTO);
+    //Serial.println(lux);
+    if (lux < 0.0001)
+        return 0.0001;
+
+    return lux;
+}
+
+
+
+float getCO2(){
+    sgp.setHumidity(currentStatus.humidity);
+    bool temp = sgp.IAQmeasure();
+
+    if (temp){
+       return sgp.eCO2;
+    }
+    return 0;
+}
+
+float getTVOC(){
+    sgp.setHumidity(currentStatus.humidity);
+    bool temp = sgp.IAQmeasure();
+
+    if (temp){
+       return sgp.TVOC;
+    }
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include "ESPAsyncWebServer.h"
+#include "DevSensors.h"
+
+
+const char* ssid = "Haveli";
+const char* password = "WelcomeToHaveli";
+
+
+
+AsyncWebServer server(80);
+
+void connectWiFi(){
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	Serial.print("Connecting to WiFi ..");
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print('.');
+		delay(1000);
+	}
+	Serial.println(WiFi.localIP());
+}
+
+
+
+void setup() {
+	Serial.begin(115200);
+	while (!Serial) { delay(10); }
+	WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.printf("WiFi Failed!\n");
+        return;
+    }
+	Serial.println(WiFi.localIP());
+	setupSGP();
+	setupLuxSensorAuto();
+	setupDHTSensor();
+	
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+	request->send_P(200, "text/plain", "33");
+	});
+	
+	server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+		request->send_P(200, "text/plain", (String(currentStatus.temperature)).c_str());
+	});
+	server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+		request->send_P(200, "text/plain", (String(currentStatus.humidity)).c_str());
+	});
+	server.on("/eco2", HTTP_GET, [](AsyncWebServerRequest *request){
+		request->send_P(200, "text/plain", (String(currentStatus.eco2)).c_str());
+	});
+	server.on("/ambientlight", HTTP_GET, [](AsyncWebServerRequest *request){
+		request->send_P(200, "text/plain", (String(currentStatus.ambientlight)).c_str());
+	});
+	server.on("/airquality", HTTP_GET, [](AsyncWebServerRequest *request){
+		request->send(200, "application/json", currentStatus.airquality);
+	});
+	
+	server.begin();
+	
+}
+
+String getAirQuality(){
+    
+    String json = "{\"air_quality\":1,\"pm10\":32,\"pm2_5\":26,\"voc\":" + String(getTVOC()) + "}";
+    return json;
+}
+
+void loop() {
+	if (WiFi.status() != WL_CONNECTED) {
+		Serial.print("Not Connected\n");
+		WiFi.disconnect();
+		WiFi.mode(WIFI_STA);
+		WiFi.begin(ssid, password);
+		if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+			Serial.printf("WiFi Failed!\n");
+			return;
+		}
+		Serial.println(WiFi.localIP());
+	}
+	Serial.println("Getting Data:");
+	currentStatus.temperature = dht.readTemperature();
+	currentStatus.humidity = dht.readHumidity();
+	currentStatus.eco2 = getCO2();
+	currentStatus.tvoc = getTVOC();	
+	currentStatus.ambientlight = getLuxDataAuto();	
+	currentStatus.airquality = getAirQuality();
+	delay(1000);
+}
+
